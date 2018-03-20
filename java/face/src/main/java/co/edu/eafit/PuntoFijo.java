d5fa331c7bebe914ddbf97b005967a06d989d3c7
Felipe Montoya Alvarez <fmontoyalvarez@gmail.com>
2018-03-20 15:39:08 -0500

    Mejorando Punto Fijo

--- java/face/src/main/java/co/edu/eafit/PuntoFijo.java
+++ java/face/src/main/java/co/edu/eafit/PuntoFijo.java
@@ -21,52 +21,34 @@ public class PuntoFijo {
 
         double feval,error, contador, xn;
         feval = this.eval.evaluatorExpression(fx,x0);
-	contador = 0.0;
-	error = tole +1;
+      	contador = 0.0;
+      	error = tole +1;
 
-	while(feval != 0 && error > tole && contador < niter){
+      	while(feval != 0 && error > tole && contador < niter){
 
-    	xn = this.eval.evaluatorExpression(gx,x0);
-    	feval = this.eval.evaluatorExpression(fx,xn);
-    	error = Math.abs(xn-x0);
-    	x0 = xn;
+          	xn = this.eval.evaluatorExpr(gx,x0);
+          	feval = this.eval.evaluatorExpr(fx,xn);
+          	error = Math.abs(xn-x0);
+          	x0 = xn;
 
-	}
-	if(feval == 0){
-	  System.out.println(x0 + " es raiz");
+      	}
+      	if(feval == 0){
+      	  System.out.println(x0 + " es raiz");
 
-	}else if (error < tole){
-		System.out.println(x0 + " Es una aproximacion con una tolerancia " + tole);
+      	}else if (error < tole){
+      		System.out.println(x0 + " Es una aproximacion con una tolerancia " + tole);
 
-	}else{
-	  System.out.println("Método fracaso en " + niter + " Iteraciones");
+      	}else{
+      	  System.out.println("Método fracaso en " + niter + " Iteraciones");
 
-	}
+      	}
 
 
     }
 
 
 
-    public void controller() throws IOException{
 
-        System.out.print("f(x) = ");
-        String fx = br.readLine();
-
-        System.out.print("x0 = ");
-        String x0Str = br.readLine();
-        double x0 = Double.parseDouble(x0Str);
-
-        System.out.print("Delta = ");
-        String deltaStr = br.readLine();
-        double delta = Double.parseDouble(deltaStr);
-
-        System.out.print("Num. Iteraciones = ");
-        String niterStr = br.readLine();
-        double niter = Double.parseDouble(niterStr);
-
-        algorithm(fx, x0, delta, niter);
-    }
 
 
 }
