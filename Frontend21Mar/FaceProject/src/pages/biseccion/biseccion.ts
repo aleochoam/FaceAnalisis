31fb2f9f837103810c48f949c7294577761156e4
csuazag <camilo.suaza12@gmail.com>
2018-03-21 22:03:56 -0500

    Subiendo frontend biseccion

--- Frontend21Mar/FaceProject/src/pages/biseccion/biseccion.ts
+++ Frontend21Mar/FaceProject/src/pages/biseccion/biseccion.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { AlertController } from 'ionic-angular';
+
 /**
  * Generated class for the BiseccionPage page.
  *
@@ -15,11 +17,50 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BiseccionPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  dataSubmit:any = {}
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+    this.dataSubmit['fx'] = '';
+    this.dataSubmit['xa'] = '';
+    this.dataSubmit['xb'] = '';
+    this.dataSubmit['nIters'] = '';
+    this.dataSubmit['tole'] = '';
+
+  }
+
+  submitForm() {
+    console.log(this.dataSubmit)
+    this.verification();
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad BiseccionPage');
+    console.log('ionViewDidLoad BusquedasIncrementalesPage');
+  }
+
+  verification(){
+   if (this.dataSubmit['fx'] == ''){
+    this.showAlert('f(x)');
+   }else if (this.dataSubmit['xa'] == ''){
+     this.showAlert('xa');
+   }else if (this.dataSubmit['xb'] == ''){
+     this.showAlert('xb');
+   }else if(this.dataSubmit['nIters'] == ''){
+    this.showAlert('Num. Iters');
+   } else if(this.dataSubmit['tole'] == ''){
+    this.showAlert('Tolerancia');
+   }else{
+    console.log("Campos verificados y completos.");
+   }   
+  }
+
+
+  showAlert(value:string) {
+    let alert = this.alertCtrl.create({
+      title: 'Error!',
+      subTitle: 'El campo ' + value + ' no puede estar vacío!',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
